4262adafbaa3fee051af54f820aecc12825c309c
Farothtc <mvkirimi@gmail.com>
2025-02-21 16:16:50 +0300

    further about express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -30,6 +30,27 @@ app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
   res.send("Hellow World");
 });
 
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(200).send("no products matched your description");
+    return res.status(200).json({ success: true, data: [] });
+  }
+  return res.status(200).json(sortedProducts);
+});
+
 app.listen(5000, () => {
   console.log("Server is listening on port 5000..");
 });
